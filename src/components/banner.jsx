ec02878a03a581fdf60f71d1e6cbdbeb15d5977d
Your Name <dksingh8268@gmail.com>
2024-10-12 10:48:32 +0530

    d

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -10,45 +10,50 @@ import "swiper/css/pagination";
 // import required modules
 import { Pagination, Autoplay } from "swiper/modules";
 import Image from "next/image";
-
 const BannerSlider = () => {
+
   return (
     <>
-      <div className="md:max-w-[100%] mx-auto pt-[82px]">
-        <Swiper
-          className="mySwiper hidden md:bg-none"
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000 }}
-          loop={true}
-          modules={[Autoplay]}
-        >
-          <SwiperSlide className="items-center md:hidden bg-[url('/images/background.jpg')] bg-cover bg-center h-screen flex justify-center">
-            <Image
-              src="/1.png" // Corrected from '/1,png' to '/1.png'
-              alt="Slide 1"
-              layout="fill" // Use layout="fill" to fill the container
-              objectFit="cover" // Ensures the image fits nicely
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/2.png"
-              alt="Slide 2"
-              layout="fill" // Use layout="fill"
-              objectFit="cover"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/3.png"
-              alt="Slide 3"
-              layout="fill" // Use layout="fill"
-              objectFit="cover"
-            />
-          </SwiperSlide>
-        </Swiper>
-      </div>
+    <div className=" md:max-w-[100%] mx-auto pt-[82px]">
+    <Swiper
+      className="mySwiper   hidden md:bg-none "
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000 }}
+      loop={true}
+      modules={[ Autoplay]}
+    >
+      <SwiperSlide className="items-center  md:hidden bg-[url('/images/background.jpg')] bg-cover bg-center h-screen flex  justify-center">
+      <Image
+      src="/1,png"
+      width={0}
+      height={0}
+      sizes="100vw"
+      style={{ width: '100%', height: 'auto' }} // optional
+    />
+      </SwiperSlide>
+      <SwiperSlide>
+      <Image
+       src="/2.png"
+       width={0}
+       height={0}
+       sizes="100vw"
+       style={{ width: '100%', height: 'auto' }} // optional
+    />
+      </SwiperSlide>
+      <SwiperSlide>
+      <Image
+        src="/3.png"
+        width={0}
+      height={0}
+      sizes="100vh"
+      style={{ width: '100%', height: 'auto' }} // optional
+    />
+      </SwiperSlide>
+    </Swiper>
+ {/* responsive */}
+ </div>
     </>
+    
   );
 };
 
